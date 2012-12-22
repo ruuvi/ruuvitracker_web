1c61fa60912073b390626ea0d7017cbb16899507
Juha Syrjälä <juha.syrjala@gmail.com>
2012-12-22 22:47:39 +0200

    use dev-server.ruuvitracker.fi domain name

--- app/js/configuration.js
+++ app/js/configuration.js
@@ -4,7 +4,7 @@ var Configuration = function() {
     this.google = {};
     this.google.analyticsId = 'UA-3439345-5'; 
     this.ruuvitracker = {};
-    this.ruuvitracker.url = 'http://198.61.201.6:8000/api/v1-dev/'
+    this.ruuvitracker.url = 'http://dev-server.ruuvitracker.fi:8000/api/v1-dev/'
     // center of Helsinki
     this.defaultLocation = new L.LatLng(60.168564, 24.941111);
     this.defaultZoom = 13;
